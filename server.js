ea6e5c951a453aa05170b0c482efef8a53ce9ea3
Shaw <shaw.s.li@gmail.com>
2016-11-03 20:04:26 +0000

    minor changes

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-console.log('hello')
+var http = require("http")
+var server = http.createServer(function(req,res){
+    //IP address, language and operating system for my browser.
+    console.log(req)
+    var ip_address = req.headers.ipaddress;
+    var language = req.headers.language;
+    var os = req.headers.operating;
+    var output = {
+        "ipaddress": ip_address,
+        "language": language,
+        "software": os
+    }
+    output = JSON.stringify(output)
+    res.end(output)
+})
+
+
+server.listen(process.env.PORT, function(){
+    console.log("Server listening on: "+ process.env.PORT);
+});
