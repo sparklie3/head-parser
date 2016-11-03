553df1917faf365bda0273b25ad596dc828e3d17
Shaw <shaw.s.li@gmail.com>
2016-11-03 20:26:19 +0000

    final changes

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var http = require("http")
 var server = http.createServer(function(req,res){
     //IP address, language and operating system for my browser.
-    console.log(req)
-    var ip_address = req.headers.ipaddress;
-    var language = req.headers.language;
-    var os = req.headers.operating;
+    //console.log(req.connection)
+    var ip_address = req.headers['x-forwarded-for'];
+    var language = req.headers['accept-language'];
+    var os = req.headers['user-agent'];
     var output = {
         "ipaddress": ip_address,
         "language": language,
